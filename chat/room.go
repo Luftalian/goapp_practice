008e2aba5306b6d73438f0174c5952df4387cb5a
Luftalian <springtechtree@gmail.com>
2022-07-01 23:54:59 +0900

    Is it ok?

--- chat/room.go
+++ chat/room.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/Luftalian/goapp_practice/trace"
 	"github.com/gorilla/websocket"
 )
 
@@ -16,6 +17,8 @@ type room struct {
 	leave chan *client
 	//clientsには参加しているすべてのクライアントが保持されます。
 	clients map[*client]bool
+	//tracerはチャットルーム上で行われた操作のログを受け取ります。
+	tracer trace.Tracer
 }
 
 func (r *room) run() {
@@ -24,20 +27,25 @@ func (r *room) run() {
 		case client := <-r.join:
 			//参加
 			r.clients[client] = true
+			r.tracer.Trace("新しいクライアントが参加しました")
 		case client := <-r.leave:
 			//退室
 			delete(r.clients, client)
 			close(client.send)
+			r.tracer.Trace("クライアントが退室しました")
 		case msg := <-r.forward:
+			r.tracer.Trace("メッセージを受信しました:", string(msg))
 			//すべてのクライアントにメッセージを転送
 			for client := range r.clients {
 				select {
 				case client.send <- msg:
-				//メッセージを送信
+					//メッセージを送信
+					r.tracer.Trace(" -- クライアントに送信されました")
 				default:
 					//送信に失敗
 					delete(r.clients, client)
 					close(client.send)
+					r.tracer.Trace(" -- 送信に失敗しました。クライアントをクリーンアップします")
 				}
 			}
 		}
@@ -67,5 +75,5 @@ func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 //r := &room{forward: make(chan []byte), join: make(chan *client), leave: make(chan *client), clients: make(map[*client]bool),}
 //newRoomはすぐに利用できる新しいチャットルームを生成して返します。
 func newRoom() *room {
-	return &room{forward: make(chan []byte), join: make(chan *client), leave: make(chan *client), clients: make(map[*client]bool)}
+	return &room{forward: make(chan []byte), join: make(chan *client), leave: make(chan *client), clients: make(map[*client]bool), tracer: trace.Off()}
 }
